0eb80fdb1ac51e520a90428068f5ea8cad9875c8
Ivo Georgiev <ivo@strem.io>
2018-05-18 13:50:14 +0200

    add iphone script

--- synaptics.sh
+++ synaptics.sh
@@ -4,7 +4,7 @@ DEVID=$(xinput list | grep Synaptics | grep -Eio '(touchpad|glidepoint)\s*id\=[0
 
 # palm detection
 xinput set-prop $DEVID "Synaptics Palm Detection" 1
-xinput set-prop $DEVID "Synaptics Palm Dimensions" 5, 5
+xinput set-prop $DEVID "Synaptics Palm Dimensions" 5, 10
 #natural scrolling
 xinput set-prop $DEVID "Synaptics Scrolling Distance" -94 -94
 # tap to click, two f fingers is right click
@@ -13,4 +13,9 @@ xinput set-prop $DEVID  "Synaptics Tap Action" 0 0 0 0 1 3 2
 # Speed
 
 # Disable while typing - workaround to touchpad not working
-syndaemon -i 0.4 -d -K -R
+syndaemon -i 0.4 -t -d -K -R
+
+
+synclient PalmDetect=1
+synclient PalmMinWidth=5
+synclient PalmMinZ=10
