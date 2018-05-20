74b58c3676fdb3b1b44461ab661bff562e89399a
Ivo Georgiev <ivo@strem.io>
2018-05-20 19:53:35 +0200

    systemBackup: quit on errors, use gzip

--- systemBackup.sh
+++ systemBackup.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -e 
+
 IMAGE_FILE=/home/ivo/storage/system-`date +"%d-%m-%Y"`
 LOOP_DEV="/dev/loop0"
 
@@ -15,18 +17,22 @@ fi
 
 # @TODO: mount existing
 
+# Prepare filesystem
 dd if=/dev/zero of="$IMAGE_FILE" bs=1M count=70000
 losetup "$LOOP_DEV" "$IMAGE_FILE"
+
 cryptsetup -vy luksFormat "$LOOP_DEV"
 cryptsetup luksOpen "$LOOP_DEV" systemVol
 
 mkfs.ext4 /dev/mapper/systemVol
-
 mount /dev/mapper/systemVol /mnt
 
+# backup
 rsync -aAX / --exclude={"/home/ivo/storage","/.snapshots/*","/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found"} /mnt
 
+# clean-up
 umount /mnt
 cryptsetup luksClose systemVol
 
-xz --format=lzma "$IMAGE_FILE"
+# compress
+gzip "$IMAGE_FILE"
